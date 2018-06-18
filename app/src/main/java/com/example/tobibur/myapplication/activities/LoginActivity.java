6e14a585ae308294fe9639d3049887960c07d39a
Tobibur <tobiburrahman786@gmail.com>
2018-06-18 17:32:20 +0530

    Added packages

---
+++ app/src/main/java/com/example/tobibur/myapplication/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.tobibur.myapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tobibur.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/tobibur/myapplication/activities/ProfileActivity.java
@@ -0,0 +1,54 @@
+package com.example.tobibur.myapplication.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tobibur.myapplication.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageView profile_dp;
+    private TextView profile_name,profile_desc;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_pro);
+        setSupportActionBar(toolbar);
+
+        profile_dp = findViewById(R.id.profileImg_id);
+        profile_name = findViewById(R.id.pro_name);
+        profile_desc = findViewById(R.id.pro_desc);
+
+
+        Intent intent = getIntent();
+
+        String pos = intent.getStringExtra("position");
+        String name = intent.getStringExtra("ProfileName");
+        String desc = intent.getStringExtra("ProfileDesc");
+
+        profile_dp.setImageResource(Integer.parseInt(pos));
+        profile_name.setText(name);
+        profile_desc.setText(desc);
+
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+
+        /*switch (pos){
+
+            case "0":
+                profile_dp.setImageResource(R.drawable.a);
+                break;
+            case "1":
+                profile_dp.setImageResource(R.drawable.b);
+                break;
+        }*/
+    }
+}
