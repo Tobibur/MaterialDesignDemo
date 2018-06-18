f0253a3169cd0a9e89803a7bf1eff2119d6992f9
Tobibur <tobiburrahman786@gmail.com>
2018-06-18 14:20:57 +0530

    Added Bottom Sheet

--- app/src/main/java/com/example/tobibur/myapplication/UIActivity.java
+++ app/src/main/java/com/example/tobibur/myapplication/UIActivity.java
@@ -1,13 +1,88 @@
 package com.example.tobibur.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 
 public class UIActivity extends AppCompatActivity {
+
+    private static final String TAG = "UIActivity";
+
+    Toolbar toolbar;
+    BottomSheetBehavior sheetBehavior;
+    LinearLayout layoutBottomSheet;
+    Button btnBottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+        btnBottomSheet = findViewById(R.id.btn_bottom_sheet);
+
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+
+        /*
+          bottom sheet state change listener
+          we are changing button text when sheet changed state
+          */
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        Log.d(TAG, "onStateChanged: STATE_HIDDEN");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        btnBottomSheet.setText("Close Sheet");
+                        Log.d(TAG, "onStateChanged: STATE_EXPANDED");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        btnBottomSheet.setText("Expand Sheet");
+                        Log.d(TAG, "onStateChanged: STATE_COLLAPSED");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        Log.d(TAG, "onStateChanged: STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        Log.d(TAG, "onStateChanged: STATE_SETTLING");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.d(TAG, "onSlide: Slided");
+            }
+        });
+
+        btnBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleBottomSheet();
+            }
+        });
+    }
+
+    public void toggleBottomSheet() {
+        if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            btnBottomSheet.setText("Close sheet");
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            btnBottomSheet.setText("Expand sheet");
+        }
     }
 }
